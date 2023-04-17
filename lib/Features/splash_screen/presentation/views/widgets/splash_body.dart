0d1cb7c0e3e556b6749d331281aea3bdbbca249a
MohamadAlnashwati <sym44759@gmail.com>
2023-04-17 10:59:43 +0300

    finish splash screen

--- lib/Features/splash_screen/presentation/views/widgets/splash_body.dart
+++ lib/Features/splash_screen/presentation/views/widgets/splash_body.dart
@@ -1,11 +1,37 @@
+import 'package:bookly_app/Features/home/data/presentation/view/home_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 import '../../../../../Core/constant/constant.dart';
 
-class SplashBody extends StatelessWidget {
+class SplashBody extends StatefulWidget {
   const SplashBody({super.key});
 
   @override
+  State<SplashBody> createState() => _SplashBodyState();
+}
+
+class _SplashBodyState extends State<SplashBody> {
+  void initState() {
+    super.initState();
+    naviateToHome();
+  }
+
+  void naviateToHome() {
+    Future.delayed(const Duration(seconds: 2), () {
+      Get.to(
+        () => const HomeScreen(),
+        transition: Transition.fade,
+        duration: const Duration(milliseconds: 500),
+      );
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
   Widget build(BuildContext context) {
     return Center(
       child: Image.asset(
